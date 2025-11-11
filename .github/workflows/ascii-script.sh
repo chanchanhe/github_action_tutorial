e1fb807a3cd94e62f9d156bca2ad772b52a98aa7
chan <cold_he22@naver.com>
2025-11-11 10:23:49 +0900

    ascii 스크립트 수정

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,5 +1,9 @@
-#/bin/sh
-sudo apt-get install cowsay -y
+#!/bin/bash
+set -e  # 에러 발생 시 스크립트 중단
+
+sudo apt-get update -y
+sudo apt-get install -y cowsay
+
 cowsay -f dragon "Run for cover, I am a DRAGON!" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
